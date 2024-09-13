776f40182db94135afa5581d34d4fac5494d4e5b
Aguspium <jakseek.jakseek@gmail.com>
2024-09-13 08:53:00 -0300

    - Liempieza de Consola

--- bot.py
+++ bot.py
@@ -5,8 +5,23 @@ import discord
 import logging
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.CRITICAL)
 
+#Configuraciones para Limpiar la consola
+logging.getLogger('discord.voice_client').setLevel(logging.CRITICAL)
+logging.getLogger('discord.gateway').setLevel(logging.CRITICAL)
+logging.getLogger('discord.http').setLevel(logging.CRITICAL)
+logging.getLogger('discord.ext.commands.bot').setLevel(logging.CRITICAL)
+logging.getLogger('discord.voice_client').setLevel(logging.CRITICAL)
+logging.getLogger('discord.voice_state').setLevel(logging.CRITICAL)
+logging.getLogger('discord.gateway').setLevel(logging.CRITICAL)
+logging.getLogger('discord.http').setLevel(logging.CRITICAL)
+logging.getLogger('discord.ext.commands.bot').setLevel(logging.CRITICAL)
+logging.getLogger('discord.player').setLevel(logging.CRITICAL)
+logging.getLogger('discord.client').setLevel(logging.CRITICAL)               
+
+#Para cargar los SlashCommands
 class MyBot(commands.Bot):
     async def setup_hook(self):
         for directory in ['./cogs']:
@@ -19,18 +34,19 @@ class MyBot(commands.Bot):
                     except Exception as e:
                         print(f'Failed to load extension {extension}: {e}')
 
+#Un loop para chequear que el bot se desconecte si no hay nadie en el canal de voz.
     @tasks.loop(minutes=5)
     async def check_voice_status(self):
         for guild in self.guilds:
             voice_client = guild.voice_client
             if voice_client and not voice_client.is_playing() and not voice_client.is_paused():
                 await voice_client.disconnect()
-                print(f"Bot desconectado de {guild.name} por inactividad.")
 
     @check_voice_status.before_loop
     async def before_check_voice_status(self):
         await self.wait_until_ready()
 
+#Prefix para comandos comunes
 bot = MyBot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
@@ -39,4 +55,4 @@ async def on_ready():
     print(f"Conectado como {bot.user}")
 
 if __name__ == "__main__":
-    bot.run(os.getenv('DB_TOKEN')) #asd
+    bot.run(os.getenv('DB_TOKEN')) 
